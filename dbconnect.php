d412865968ecf6dcb8abb71cae3520bd74811aeb
zetsuboy <57826001+zetsuboy@users.noreply.github.com>
2021-11-30 13:30:52 +1000

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'test_for_buttons';
-$username = 'root';
-$password = '';
-
-$conn = mysqli_connect($host, $username, $password, $dbname);
-$conn->set_charset("utf8");
-?>
